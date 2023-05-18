8d5beea46b7fad1683911615d887922b0528d957
Moises Sacal <moisbo@gmail.com>
2023-05-18 12:51:34 +1000

    cleanup

--- portal/src/components/Footer.component.vue
+++ portal/src/components/Footer.component.vue
@@ -6,10 +6,19 @@
         <a class="text-gray-600 font-semibold" :href="href">{{ hrefText }}</a>
       </el-col>
     </el-row>
-    <el-row v-if="terms">
+    <el-row>
       <el-col :span="24" class="flex space-x-4">
-        <el-link class="text-gray-600 font-semibold" :href="terms.href" :underline="false">{{ terms.title }}</el-link>
-        <el-link class="text-gray-600 font-semibold" @click="splash=true" :underline="false">
+        <el-link v-if="terms"
+                 class="text-gray-600 font-semibold"
+                 :href="terms?.href"
+                 :underline="false">{{
+            terms?.title
+          }}
+        </el-link>
+        <el-link v-if="splashLauncher"
+                 class="text-gray-600 font-semibold"
+                 @click="splash=true"
+                 :underline="false">
           {{ splashLauncher || 'Splash' }}
         </el-link>
       </el-col>
@@ -28,9 +37,9 @@ export default {
       copyright: this.$store.state.configuration.ui.footer.copyright,
       href: this.$store.state.configuration.ui.footer.link.href,
       hrefText: this.$store.state.configuration.ui.footer.link.text,
-      terms: this.$store.state.configuration.ui?.terms,
+      terms: this.$store.state.configuration.ui?.terms || null,
       splash: false,
-      splashLauncher: this.$store.state.configuration.ui?.splashLauncher
+      splashLauncher: this.$store.state.configuration.ui?.splashLauncher || null
     }
   }
 }
