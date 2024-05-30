0bbb3395010f750f1e6347679983ebbfb64bc567
Moises Sacal <moisbo@gmail.com>
2024-05-30 14:21:33 +1000

    updated text

--- portal/src/components/SearchDetailElement.component.vue
+++ portal/src/components/SearchDetailElement.component.vue
@@ -75,8 +75,8 @@
         </el-row>
         <el-row v-if="types && types.includes('RepositoryCollection')">
           <span v-if="!isEmpty(subCollections)">Collections: {{ subCollections?.total }},&nbsp;</span>
-          <span>Objects: {{ total }},&nbsp;</span>
-          <span v-if="typeFile">Files: {{ typeFile?.['doc_count'] }}</span>
+          <span v-if="total > 0">Objects: {{ total }}</span>
+          <span v-if="typeFile">,&nbsp;Files: {{ typeFile?.['doc_count'] }}</span>
         </el-row>
         <el-row :align="'middle'" v-if="highlight">
           <ul>
