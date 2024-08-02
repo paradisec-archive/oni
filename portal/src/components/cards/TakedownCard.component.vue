be4bd5d304842b114e2e81a7c3b1bf550dd7356e
Rosanna Smith <rosanna.smith@uq.edu.au>
2024-08-02 12:11:40 +1000

    takedown functionality added

---
+++ portal/src/components/cards/TakedownCard.component.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-card :body-style="{ padding: '0px' }" class="mx-10 p-5" v-if="this.takedownForm">
+    <h5 class="text-2xl font-medium">Takedown Request</h5>
+    <hr class="divider divider-gray pt-2" />
+    <h4 class="text-1xl font-medium">
+      If you see an item on this page that you think should not be made public, you can request that it be taken
+      down:
+      <el-link underline="underline" :underline="true" type="primary" :href="takedownLink()" target="_blank"
+        class="mx-1">
+        Takedown Request Form
+      </el-link>
+    </h4>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      takedownForm: this.$store.state.configuration.ui.googleForm?.takedown
+    }
+  },
+  methods: {
+    takedownLink() {
+      let currentUrl = encodeURIComponent(window.location.href);
+      const form = this.takedownForm;
+      return `${form}${currentUrl}`
+    }
+  }
+}
+</script>
